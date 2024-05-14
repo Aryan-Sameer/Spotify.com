3ffd589b4787c264998d15a287843dcddf281ea4
Aryan-Sameer <138378287+Aryan-Sameer@users.noreply.github.com>
2024-05-14 18:57:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currFolder
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`https://aryan-sameer.github.io/Spotify.com/songs/${folder}`)
+    let a = await fetch(`/Spotify.com/songs/${folder}`)
     let response = await a.text()
     let div = document.createElement('div')
     div.innerHTML = response
@@ -58,7 +58,7 @@ const playMusic = (track, pause = false) => {
 
 
 async function displayAlbums() {
-    let a = await fetch(`https://aryan-sameer.github.io/Spotify.com/songs/`)
+    let a = await fetch(`/Spotify.com/songs/`)
     let response = await a.text()
     let div = document.createElement('div')
     div.innerHTML = response
@@ -70,7 +70,7 @@ async function displayAlbums() {
 
         if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)
-            let a = await fetch(`https://aryan-sameer.github.io/Spotify.com/songs/${folder}/info.json`)
+            let a = await fetch(`/Spotify.com/songs/${folder}/info.json`)
             let response = await a.json()
             cardContainer.innerHTML +=
                 `<div data-folder="${folder}" class="card">
